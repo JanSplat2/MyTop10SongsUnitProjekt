4c4bc09fbaa232efd10a888f0ccf7b64c8d286ff
JanSplat2 <jan.splat@outlook.de>
2025-09-08 14:47:49 -0600

    Initial Commit

---
+++ MyTop10SongsUnitProjekt/MyTop10SongsUnitProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTop10SongsUnitProjektApp.swift
+//  MyTop10SongsUnitProjekt
+//
+//  Created by Dittrich, Jan - Student on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTop10SongsUnitProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyTop10SongsUnitProjektTests/MyTop10SongsUnitProjektTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyTop10SongsUnitProjektTests.swift
+//  MyTop10SongsUnitProjektTests
+//
+//  Created by Dittrich, Jan - Student on 9/8/25.
+//
+
+import Testing
+@testable import MyTop10SongsUnitProjekt
+
+struct MyTop10SongsUnitProjektTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
