bc691f221429fcdd581263d9301aa2c54ba76715
JanSplat2 <jan.splat@outlook.de>
2025-09-09 09:03:58 -0600

    MVP and 2nd stretch

---
+++ MyTop10SongsUnitProjekt/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  MyTop10SongsUnitProjekt
+//
+//  Created by Dittrich, Jan - Student on 9/8/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
