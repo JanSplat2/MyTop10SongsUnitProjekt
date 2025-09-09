1be78cb021af5aa1e2d00db97d008df95ed86cd6
JanSplat2 <jan.splat@outlook.de>
2025-09-09 13:57:10 -0600

    if you click on the songcovers, you get directed to the spotify website. ONLY when you are in the simulator!!!! Next stretch: Create a way for the user to switch between to a seperate view, where they can scroll horizontaly

--- MyTop10SongsUnitProjekt/CardView.swift
+++ MyTop10SongsUnitProjekt/CardView.swift
@@ -29,6 +29,7 @@ struct CardView: View {
     var whyY : CGFloat
     var answerX : CGFloat
     var answerY : CGFloat
+    var spotifyURL: String
     
     var body: some View {
         
@@ -42,34 +43,43 @@ struct CardView: View {
                     )
                 )
                 .frame(width: 360,height: 700)
+            
             Text(number)
                 .font(.system(size: 45, weight: .bold))
                 .foregroundColor(textColor1)
                 .offset(x: numberX ,y: numberY)
+            
             Text(songName)
                 .font(.system(size: 35, weight: .light))
                 .foregroundColor(textColor1)
                 .offset(x: songNameX,y: songNameY)
+            
             Text(CreatorName)
                 .font(.system(size: 25, weight: .light))
                 .foregroundColor(textColor1)
                 .offset(x: creatorNameX,y: creatorNameY)
-            Image(cover)
-                .resizable()
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .frame(width: 310, height: 310)
+            
+            Link(destination: URL(string: spotifyURL)!) {
+                Image(cover)
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .frame(width: 310, height: 310)
+            }
+            
             Text(why)
                 .font(.system(size: 20, weight: .bold))
                 .foregroundColor(textColor2)
                 .offset(x: whyX, y: whyY)
+            
             Text(answer)
                 .font(.system(size: 20, design: .serif))
                 .foregroundColor(textColor2)
                 .offset(x: answerX, y: answerY)
+            
         }
     }
 }
 
 #Preview {
-    CardView(number: "#10", cover: "GodsPlan", color1: .black, color2: .white, textColor1: .white, textColor2: .black, songName: "God's Plan", CreatorName: "Drake", why: "Why This Song?", answer: "I've heard it enough to like it\n(somehow)", numberX: -110, songNameX: -75, creatorNameX: -120, numberY: -300, songNameY: -260, creatorNameY: -225, whyX: -75, whyY: 180, answerX: -25,answerY: 220)
+    CardView(number: "#10", cover: "GodsPlan", color1: .black, color2: .white, textColor1: .white, textColor2: .black, songName: "God's Plan", CreatorName: "Drake", why: "Why This Song?", answer: "I've heard it enough to like it\n(somehow)", numberX: -110, songNameX: -75, creatorNameX: -120, numberY: -300, songNameY: -260, creatorNameY: -225, whyX: -75, whyY: 180, answerX: -25,answerY: 220,spotifyURL: "https://open.spotify.com/track/6DCZcSspjsKoFjzhevrjNN")
 }
